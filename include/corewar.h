98b016ef71c972b88baaa4319d2b04e4a693b148
mathis.haba <mathis1.haba@epitech.eu>
2026-05-01 18:13:22 +0200

    {-} reorganize includes and function declarations in corewar.h

--- include/corewar.h
+++ include/corewar.h
@@ -4,11 +4,18 @@
 ** File description:
 ** the .h for corewar
 */
-#include "./op.h"
 
 #ifndef COREWAR_H_
     #define COREWAR_H_
 
+    #include "./op.h"
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <unistd.h>
+    #include <fcntl.h>
+    #include <stdint.h>
 
+int load_champion_header(const char *filepath);
+int swap_int32(int val);
 
 #endif
