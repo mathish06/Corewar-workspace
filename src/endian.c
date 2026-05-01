02f9dde41ac3e5c912d9c24a09c065d7848b4014
mathis.haba <mathis1.haba@epitech.eu>
2026-05-01 18:14:02 +0200

    {+}  endian.c for endianness swap utility

---
+++ src/endian.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2026
+** endian.c
+** File description:
+** Endianness swap utility for corewar
+*/
+
+#include "../include/corewar.h"
+
+int swap_int32(int val)
+{
+    uint32_t v = (uint32_t)val;
+    uint32_t res = (v >> 24)
+        | ((v << 8) & 0xff0000)
+        | ((v >> 8) & 0xff00)
+        | (v << 24);
+
+    return (int)res;
+}
