fe553992e9c0e3d434955367e7a732c2eb694895
mathis.haba <mathis1.haba@epitech.eu>
2026-05-10 12:29:45 +0200

    {~} refactor unit tests to validate endianness with swap_int32 function

--- tests/unit_tests.c
+++ tests/unit_tests.c
@@ -1,5 +1,8 @@
 #include <criterion/criterion.h>
+#include "../include/corewar.h"
 
-Test(dummy, basic) {
-    cr_assert_eq(1, 1);
+Test(endianness, swap_int32) {
+    int val = 0x12345678;
+    int expected = 0x78563412;
+    cr_assert_eq(swap_int32(val), expected);
 }
