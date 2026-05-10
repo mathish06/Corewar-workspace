2ace21479943c72828bd48da9ef086cd25a03f36
Loan Besse <loan.besse@epitech.eu>
2026-05-10 21:25:10 +0200

    {+} Added the static function "handle_n_flag" to handle the -n

--- src/core/corewar.c
+++ src/core/corewar.c
@@ -20,6 +20,20 @@ static int handle_dump(char **argv, int *i, global_t *global)
     return 0;
 }
 
+static int handle_n_flag(char **argv, int *i, int *temp_n)
+{
+    int nbr;
+
+    if (argv[*i + 1] == NULL)
+        return 84;
+    if (my_str_isnum(argv[*i + 1]) == 0)
+        return 84;
+    nbr = my_getnbr(argv[*i + 1]);
+    *temp_n = nbr;
+    (*i)++;
+    return 0;
+}
+
 int corewar(int argc, char **argv)
 {
     global_t global;
