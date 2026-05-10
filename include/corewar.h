2f147177d98ec651910a1f0271c74b11bbdc75de
Loan Besse <loan.besse@epitech.eu>
2026-05-10 21:19:24 +0200

    {~} removal of the champion structure and addition of the prototype of the new functions

--- include/corewar.h
+++ include/corewar.h
@@ -16,21 +16,16 @@
     #include <stdint.h>
     #include <./corwar_struct.h>
 
-typedef struct champion_s {
-    header_t header;
-    uint8_t *code;
-    int load_address;
-    int prog_number;
-} champion_t;
-
 int load_champion(champion_t *champ, const char *filepath);
 void free_champion(champion_t *champ);
 int swap_int32(int val);
 
 int my_str_isnum(char *str);
 
+int corewar(int argc, char **argv);
 
 
 int my_strcmp(char const *s1, char const *s2);
+int my_getnbr(char const *str);
 
 #endif
