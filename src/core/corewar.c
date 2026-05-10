ed8a72da9a5dd272d8b0c8472daab993ef2d2def
Loan Besse <loan.besse@epitech.eu>
2026-05-10 21:21:47 +0200

    {+} Added the static function "handle_dump" to handle the dump

--- src/core/corewar.c
+++ src/core/corewar.c
@@ -6,19 +6,29 @@
 */
 #include "../../include/corewar.h"
 
+static int handle_dump(char **argv, int *i, global_t *global)
+{
+    int nbr;
+
+    if (argv[*i + 1] == NULL)
+        return 84;
+    if (my_str_isnum(argv[*i + 1]) == 0)
+        return 84;
+    nbr = my_getnbr(argv[*i + 1]);
+    global->dump = nbr;
+    (*i)++;
+    return 0;
+}
+
 int corewar(int argc, char **argv)
 {
     global_t global;
-    int count = 0;
+    int temp_n = -1;
+    int temp_a = -1;
 
     global.dump = -1;
-    global.champ_list->flag_a = -1;
-    global.champ_list->flag_n = -1;
-    for (; argv != NULL; count++) {
-        if (my_strcmp("-dump", argc[count]) == 0 && argc[count++] != NULL) {
-            if (my_str_isnum(argc[count++]) == 1) {
-                
-            }
-        }
+    global.nbr_champions = 0;
+    for (int i = 1; i < argc; i++) {
+        
     }
 }
