4f5175b4b05075b1b3c94094608c7f8cbbc0eb44
Loan Besse <loan.besse@epitech.eu>
2026-05-11 14:50:10 +0200

    {+} Added the "entry_into_arena" function which allows copying each field->code byte into vm->arena

--- src/champ/prog_champ.c
+++ src/champ/prog_champ.c
@@ -11,7 +11,7 @@ void prep_usernames_and_addresses(global_t *global)
     int champ_nbr = 1;
     int gap = MEM_SIZE / global->nbr_champions;
 
-    for (int i = 0; i <= global->nbr_champions; i++) {
+    for (int i = 0; i < global->nbr_champions; i++) {
         if (global->champions[i].flag_n == -1) {
             global->champions[i].prog_number = champ_nbr;
             champ_nbr++;
@@ -19,7 +19,7 @@ void prep_usernames_and_addresses(global_t *global)
             global->champions[i].prog_number = global->champions[i].flag_n;
         }
     }
-    for (int i = 0; i <= global->nbr_champions; i++) {
+    for (int i = 0; i < global->nbr_champions; i++) {
         if (global->champions[i].flag_a == -1)
             global->champions[i].load_address = i * gap;
         else {
@@ -28,3 +28,18 @@ void prep_usernames_and_addresses(global_t *global)
         }
     }
 }
+
+void entry_into_arena(global_t *global, vm_t *vm)
+{
+    int start;
+    int dest;
+
+    for (int i = 0; i < global->nbr_champions; i++) {
+        start = global->champions[i].load_address;
+        for (int y = 0; y < global->champions[i].header.prog_size; y++) {
+            dest = (start + y) % MEM_SIZE;
+            vm->arena[dest] = global->champions[i].code[y];
+        }
+    }
+}
+
