e9dcfa02f45bc94364dcb5843e5d0ddc692408a1
mathis.haba <mathis1.haba@epitech.eu>
2026-05-11 02:05:20 +0200

    {+}  unit test for initializing VM memory to zeros

--- tests/unit_tests.c
+++ tests/unit_tests.c
@@ -6,3 +6,15 @@ Test(endianness, swap_int32) {
     int expected = 0x78563412;
     cr_assert_eq(swap_int32(val), expected);
 }
+
+Test(arena, init_vm_zeros_memory) {
+    vm_t vm;
+
+    for (int i = 0; i < MEM_SIZE; i++) {
+        vm.arena[i] = 42;
+    }
+    init_vm(&vm);
+    for (int i = 0; i < MEM_SIZE; i++) {
+        cr_assert_eq(vm.arena[i], 0);
+    }
+}
