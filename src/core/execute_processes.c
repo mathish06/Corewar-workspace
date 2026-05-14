10f652b5ea2fd3c0d009a124ba38126dfdd6f499
Loan Besse <loan.besse@epitech.eu>
2026-05-14 15:35:07 +0200

    {+} Creation of the file "execute_processes.c" & static function that looks at the arena cell & retrieves the casting time from op_tab

---
+++ src/core/execute_processes.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2026
+** execute_processes.c
+** File description:
+** execute_processes for corewar
+*/
+#include "../../include/corewar.h"
+
+static void fetch_instruction(vm_t *vm, process_t *curr)
+{
+    if (curr->cycle_to_wait == 0) {
+        curr->current_opcode = vm->arena[curr->pc];
+        if (curr->current_opcode >= 1 && curr->current_opcode <= 16) {
+            curr->cycle_to_wait = op_tab[curr->current_opcode].nbr_cycles;
+        }
+    }
+}
