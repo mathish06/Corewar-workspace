03540f6d5aecf1f958d065d145f787b5b18c71b5
mathis.haba <mathis1.haba@epitech.eu>
2026-05-14 10:47:40 +0200

    {~}  replace fprintf with custom error output function in main

--- src/main.c
+++ src/main.c
@@ -2,16 +2,23 @@
 ** EPITECH PROJECT, 2026
 ** main.c
 ** File description:
-** Main entry
+** main
 */
 
 #include "corewar.h"
 
+static void my_putstr_err(const char *str)
+{
+    for (int i = 0; str && str[i] != '\0'; i++)
+        write(2, &str[i], 1);
+}
+
 int main(int argc, char **argv)
 {
     if (argc < 2) {
-        fprintf(stderr, "Usage: %s [-dump N] [-n N champ.cor]", argv[0]);
-        fprintf(stderr, " [-a N champ.cor] ...\n");
+        my_putstr_err("Usage: ");
+        my_putstr_err(argv[0]);
+        my_putstr_err(" [-dump N] [-n N champ.cor] [-a N champ.cor] ...\n");
         return 84;
     }
     return corewar(argc, argv);
