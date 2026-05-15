def2f6cb93196d8dacd95e416ec28af01446bef0
Loan Besse <loan.besse@epitech.eu>
2026-05-15 11:18:55 +0200

    {+} Added the exec_live function which allows a champion to announce that they are alive

---
+++ src/actions/basic_instructions.c
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2025
+** basic_instructions.c
+** File description:
+** basic_instructions for corewar
+*/
+#include "../../include/corewar.h"
+
+void exec_live(global_t *global, vm_t *vm, process_t *curr)
+{
+    int player_nb;
+    int idx = (curr->pc + 1) % MEM_SIZE;
+
+    player_nb = (vm->arena[idx] << 24)
+        | (vm->arena[(idx + 1) % MEM_SIZE] << 16)
+        | (vm->arena[(idx + 2) % MEM_SIZE] << 8)
+        | (vm->arena[(idx + 3) % MEM_SIZE]);
+    curr->is_alive = 1;
+    vm->live_count += 1;
+    for (int i = 0; i < global->nbr_champions; i++) {
+        if (global->champions[i].prog_number == player_nb) {
+            my_putstr("The player ");
+            my_put_nbr(global->champions[i].prog_number);
+            my_putstr("(");
+            my_putstr(global->champions[i].header.prog_name);
+            my_putstr(")");
+            my_putstr(" is alive.\n");
+        }
+    }
+    curr->pc = (curr->pc + 5) % MEM_SIZE;
+}
